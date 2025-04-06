8eb3bc30c1fb8073df6cf6dfde5ef741c44eb773
Mohamed Gamal <mohamedgamalz3731@gmail.com>
2025-04-06 22:38:23 +0200

    feat(footer): get data from endpoint

--- src/app/_components/MyFooter.tsx
+++ src/app/_components/MyFooter.tsx
@@ -4,8 +4,23 @@ import { MdArrowOutward } from "react-icons/md";
 import FooterSlider from "./FooterSlider";
 import { bigShouldersInline } from "../Fonts";
 
-
-const MyFooter = () => {
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+interface IFooterData {
+  _id: string;
+  email: string;
+  phone: string;
+  all_rights: string;
+  based_in: string;
+  social_media: {
+    _id: string;
+    name: string;
+    url: string;
+  }[];
+}
+const MyFooter =async () => {
+  const res = await fetch(`${apiUrl}/api/footer`);
+  const data:IFooterData[] = await res.json();
+  console.log(data[0]);
   return (
     <>
     <section
@@ -26,36 +41,26 @@ const MyFooter = () => {
             <h4 className="font-semibold text-[#515151] mb-3">
               REACH OUT TO US
             </h4>
-            <p className="md:text-[1.5rem]">(+20) 1204811102</p>
-            <p className="md:text-[1.5rem]">mohamedgamalz3731@gmail.com</p>
+            <p className="md:text-[1.5rem]">(+20) {data[0].phone}</p>
+            <p className="md:text-[1.5rem]">{data[0].email}</p>
           </div>
           <div className="cols-span-12 md:col-span-1 flex flex-col justify-between items-center md:items-end">
             <h4 className="self-s font-semibold text-[#515151] mb-3">SOCIAL</h4>
             <ul className="flex flex-col items-end gap-3">
-              <li>
-                <a className="flex gap-1 items-center" href="">
-                  <MdArrowOutward />
-                  LINKEDIN
+              {data[0].social_media.map((social) => (
+                <li key={social._id}>
+                  <a className="flex gap-1 items-center" href={social.url}>
+                    <MdArrowOutward />
+                    {social.name}
                 </a>
               </li>
-              <li>
-                <a className="flex gap-1 items-center" href="">
-                  <MdArrowOutward />
-                  WHATSAPP
-                </a>
-              </li>
-              <li>
-                <a className="flex gap-1 items-center" href="">
-                  <MdArrowOutward />
-                  FACEBOOK
-                </a>
-              </li>
+              ))}
             </ul>
           </div>
         </div>
         <div className="font-semibold text-[#515151] flex flex-col md:flex-row items-center md:justify-between justify-center mt-10">
-        <p>© 2024 ALL RIGHTS RESERVED</p>
-        <p>BASED IN HANOI, VIETNAM</p>
+        <p>{data[0].all_rights}</p>
+        <p>{data[0].based_in}</p>
         </div>
       </div>
         

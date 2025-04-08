a464d6b14254b829e9e100a4f8b70e76086fc4e8
Mohamed Gamal <mohamedgamalz3731@gmail.com>
2025-04-08 16:42:07 +0200

    style(dataSlider): change style

--- src/app/_components/DataSlider.tsx
+++ src/app/_components/DataSlider.tsx
@@ -2,8 +2,8 @@
 import Image from "next/image";
 import CountUp from "./CountUp";
 import img1 from "../../../assets/1.jpg";
-import img2 from "../../../assets/1_New1.jpg";
-import img3 from "../../../assets/2.jpg";
+// import img2 from "../../../assets/1_New1.jpg";
+// import img3 from "../../../assets/2.jpg";
 import { useKeenSlider } from "keen-slider/react";
 import { useEffect } from "react";
 
@@ -11,7 +11,7 @@ const animation = { duration: 50000, easing: (t: number) => t };
 const DataSlider = () => {
   const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
     slides: {
-      perView: 8,
+      perView: 9,
       spacing: 0,
     },
     loop: true,
@@ -36,23 +36,33 @@ const DataSlider = () => {
     }
   }, [ instanceRef]);
   return (
-    <div className="relative border-t-[1px] border-b-[1px] border-[#222832] h-[50vh] overflow-hidden">
-        <div className="dataSlider-container flex flex-col gap-7 h-full z-[1]">
-          {[img1, img2, img3].map((img, rowIndex) => (
-            <div key={rowIndex} ref={sliderRef} className="keen-slider dataSlide !overflow-visible z-[2] mb-4">
-              {[...Array(10)].map((_, i) => (
+    <div className="relative border-t-[1px] border-b-[1px] border-[#eef4fd] dark:border-[#222832] h-[40vh] overflow-hidden">
+        <div className="dataSlider-container flex flex-col items-center justify-s gap-7 h-full z-[1]">
+            <div  ref={sliderRef} className="keen-slider dataSlide !overflow-visible z-[2] mb-4">
+              {[...Array(20)].map((_, i) => (
                 <div key={i} className="keen-slider__slide !overflow-visible relative">
-                  <Image
-                    src={img}
-                    alt=""
-                    className="w-[90px] h-full object-cover"
-                  />
+                  <div className="transform rotate-45 flex flex-col gap-4">
+                    <Image
+                      src={img1}
+                      alt=""
+                      className="w-[100px] h-full object-cover"
+                    />
+                    <Image
+                      src={img1}
+                      alt=""
+                      className="w-[100px] h-full object-cover"
+                    />
+                    <Image
+                      src={img1}
+                      alt=""
+                      className="w-[100px] h-full object-cover"
+                    />
+                  </div>
                 </div>
               ))}
             </div>
-          ))}
         </div>
-      <div className="py-10 bg-[#00000058] absolute top-0 left-0 right-0 bottom-0 z-[3]">
+      <div className="py-10 bg-[#ffffffc3] dark:bg-[#000000c3] absolute top-0 left-0 right-0 bottom-0 z-[3]">
         <div className="flex justify-center items-center h-full gap-52 text-[270%] font-[700]">
           <div className="flex flex-col items-center">
             <CountUp

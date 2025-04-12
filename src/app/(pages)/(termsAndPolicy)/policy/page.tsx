24484e76b19f2a7505910d7e02ed43dfb09343c1
Mohamed Gamal <mohamedgamalz3731@gmail.com>
2025-04-12 17:57:07 +0200

    feat(termsCollection): add endpoints to be ready for backend

--- src/app/(pages)/(termsAndPolicy)/policy/page.tsx
+++ src/app/(pages)/(termsAndPolicy)/policy/page.tsx
@@ -1,11 +1,15 @@
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-const page = () => {
+const page = async () => {
+  const res = await fetch(`${apiUrl}/api/privacy-policy`);
+    const data = await res.json();
+    console.log(data);
+
   return (
     <div className='container mx-auto px-4 md:px-0 py-10'>
       <h1 className='text-3xl font-bold text-center mb-10'>Privacy Policy</h1>
-      <p>Welcome to our website. If you continue to browse and use this website, you are agreeing to comply with and be bound by the following terms and conditions of use, which together with our privacy policy govern our relationship with you in relation to this website. If you disagree with any part of these terms and conditions, please do not use our website.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, a! Nihil minima ullam quod quam ducimus corrupti iusto omnis voluptatibus! Adipisci, asperiores rerum beatae rem non ut itaque iusto incidunt laborum repellendus hic odit et nulla deleniti optio velit unde, aliquam quod voluptatibus dolor dolorum officiis animi nihil. Soluta blanditiis eius facere voluptatibus placeat quos illo id hic minima nisi, magni vero cum dolorem enim ad, explicabo laudantium possimus similique eveniet eum! Debitis alias earum dicta sunt laborum qui quas est nobis corrupti excepturi expedita molestiae quasi ex eius labore quo reiciendis voluptate, et, obcaecati recusandae ipsum fugiat distinctio accusantium.</p>
+      <p  dangerouslySetInnerHTML={{ __html: data }}/>
     </div>
   )
 }

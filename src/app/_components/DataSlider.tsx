d48d10a3c00ab619543285ddc26ca92531c690dc
Mohamed Gamal <mohamedgamalz3731@gmail.com>
2025-04-14 15:16:45 +0200

    style(inforamtionSlider): modify the slider

--- src/app/_components/DataSlider.tsx
+++ src/app/_components/DataSlider.tsx
@@ -1,14 +1,20 @@
 "use client";
 import Image from "next/image";
 import CountUp from "./CountUp";
-import img1 from "../../../assets/1.jpg";
-// import img2 from "../../../assets/1_New1.jpg";
-// import img3 from "../../../assets/2.jpg";
 import { useKeenSlider } from "keen-slider/react";
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 const animation = { duration: 50000, easing: (t: number) => t };
+
+interface ItemType {
+  _id: string;
+  image: string;
+  isHidden: boolean;
+}
+
 const DataSlider = () => {
+  const [imgs, setImgs] = useState<ItemType[]>([]);
   const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
     slides: {
       perView: 9,
@@ -30,40 +36,79 @@ const DataSlider = () => {
       }
     },
   });
+
+  // Fetch images from API
+  const getImages = useCallback(() => {
+    fetch(`${apiUrl}/api/images`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data.length);
+        if (data.length < 20) {
+          const duplicatedData = duplicateArrayToLength(data, 20);
+          setImgs(duplicatedData);
+        } else {
+          setImgs(data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, [apiUrl]);
+
   useEffect(() => {
-    if (instanceRef.current) {
-      instanceRef.current.update();
+    getImages();
+  }, [getImages]);
+
+  useEffect(() => {
+    if (instanceRef.current && imgs.length > 0) {
+      instanceRef.current.update(); // Reinitialize the slider
+      instanceRef.current?.moveToIdx(5, true, animation); // Start animation
     }
-  }, [ instanceRef]);
+  }, [imgs, instanceRef]);
+
+  function duplicateArrayToLength(array: ItemType[], targetLength = 20): ItemType[] {
+    if (array.length >= targetLength) {
+      return array.slice(0, targetLength);
+    }
+
+    const duplicatedArray = [];
+    while (duplicatedArray.length < targetLength) {
+      duplicatedArray.push(...array);
+    }
+
+    return duplicatedArray.slice(0, targetLength);
+  }
+
   return (
     <div className="relative border-t-[1px] border-b-[1px] border-[#eef4fd] dark:border-[#222832] h-[40vh] overflow-hidden">
-        <div className="dataSlider-container flex flex-col items-center justify-s gap-7 h-full z-[1]">
-            <div  ref={sliderRef} className="keen-slider dataSlide !overflow-visible z-[2] mb-4">
-              {[...Array(20)].map((_, i) => (
-                <div key={i} className="keen-slider__slide !overflow-visible relative">
-                  <div className="transform rotate-45 flex flex-col gap-4">
-                    <Image
-                      src={img1}
-                      alt=""
-                      className="w-[100px] h-full object-cover"
-                    />
-                    <Image
-                      src={img1}
-                      alt=""
-                      className="w-[100px] h-full object-cover"
-                    />
-                    <Image
-                      src={img1}
-                      alt=""
-                      className="w-[100px] h-full object-cover"
-                    />
-                  </div>
+      <div className="dataSlider-container flex flex-col items-center justify-s gap-7 h-full z-[1]">
+        <div ref={sliderRef} className="keen-slider dataSlide !overflow-visible z-[2] mb-4">
+          {/* Group images into sets of three for each slide */}
+          {Array.from({ length: Math.ceil(imgs.length ) }).map((_, groupIndex) => {
+            const startIndex = groupIndex ; // Starting index for the group
+            const group = imgs.slice(startIndex, startIndex + 3); // Get 3 images for the group
+
+            return (
+              <div key={groupIndex} className="keen-slider__slide !overflow-visible relative">
+                <div className="transform rotate-45 flex flex-col gap-4">
+                  {group.map((img, rowIndex) => (
+                    <div key={rowIndex} className="relative w-[110px] h-[120px]">
+                      <Image
+                        src={img.image}
+                        alt=""
+                        fill
+                        className="object-cover"
+                      />
+                    </div>
+                  ))}
                 </div>
-              ))}
-            </div>
+              </div>
+            );
+          })}
         </div>
+      </div>
       <div className="py-10 bg-[#ffffffc3] dark:bg-[#000000c3] absolute top-0 left-0 right-0 bottom-0 z-[3]">
-        <div className="flex justify-center items-center h-full gap-52 text-[270%] font-[700]">
+        <div className="flex justify-center items-center h-full gap-52 text-[250%] font-[700]">
           <div className="flex flex-col items-center">
             <CountUp
               from={0}
@@ -84,7 +129,7 @@ const DataSlider = () => {
               duration={1}
               className="count-up-text"
             />
-            <div>All Projects</div>
+            <div>Number Of Clients</div>
           </div>
           <div className="flex flex-col items-center">
             <CountUp
@@ -104,14 +149,3 @@ const DataSlider = () => {
 };
 
 export default DataSlider;
-
-//   {[...Array(1)].flatMap(() =>
-//     [...Array(5)].map((_, index) => (
-//       <Image
-//         src={img}
-//         alt=""
-//         className="w-[120px] h-full object-cover"
-//         key={`${rowIndex}-${index}-${Math.random()}`} // unique key
-//       />
-//     ))
-//   )}
